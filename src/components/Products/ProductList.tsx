00010d0a7912a0d4d92b29e97ef316f4d66359c9
Yaroslav <kznkv.test@gmail.com>
2023-12-26 19:13:11 +0200

    added image to product and product-quantity

--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -18,7 +18,15 @@ const ProductList = (props: Props) => {
             </Typography>
             <Grid container spacing={4}>
                 {productsArray.map(
-                    ({ id, title, description, capacity, price, type }) => (
+                    ({
+                        id,
+                        title,
+                        description,
+                        capacity,
+                        price,
+                        type,
+                        image,
+                    }) => (
                         <Grid item xs={12} sm={6} md={4} key={id}>
                             <ProductListItem
                                 title={title}
@@ -26,6 +34,7 @@ const ProductList = (props: Props) => {
                                 capacity={capacity}
                                 type={type}
                                 price={price}
+                                image={image}
                             />
                         </Grid>
                     )
