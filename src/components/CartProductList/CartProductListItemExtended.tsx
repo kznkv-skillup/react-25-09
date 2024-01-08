729119f482668da3ac6909cd45825d905ab76622
Yaroslav <kznkv.test@gmail.com>
2024-01-08 19:48:28 +0200

    added CartProductListItemExtended

---
+++ src/components/CartProductList/CartProductListItemExtended.tsx
@@ -0,0 +1,26 @@
+import { Card, CardContent, Grid } from '@mui/material'
+import { Product } from 'utils/productsArray'
+
+type Props = {
+    product: Product
+    productCount: number
+}
+
+const CartProductListItemExtended = ({ product, productCount }: Props) => {
+    return (
+        <Grid item xs={12} sm={6} md={4}>
+            <Card variant="outlined">
+                <CardContent>
+                    <div className="product-img">
+                        <img src={product.image} alt={product.title} />
+                    </div>
+                    <div>{product.title}</div>
+                    <p>Price for one item: {product.price}</p>
+                    <p>Count: {productCount}</p>
+                </CardContent>
+            </Card>
+        </Grid>
+    )
+}
+
+export default CartProductListItemExtended
--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,5 +1,6 @@
-import { Typography } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import CartProductList from 'components/CartProductList/CartProductList'
+import CartProductListItemExtended from 'components/CartProductList/CartProductListItemExtended'
 import CartTotal from 'components/CartTotal/CartTotal'
 
 type Props = {
@@ -14,7 +15,13 @@ const CartPage = ({ productsInCart }: Props) => {
             <Typography component={'h1'} variant="h4">
                 Cart
             </Typography>
-            <CartProductList productsInCart={productsInCart} />
+            <Grid container spacing={4}>
+                <CartProductList
+                    productsInCart={productsInCart}
+                    CartItem={CartProductListItemExtended}
+                />
+            </Grid>
+
             <CartTotal productsInCart={productsInCart} />
         </div>
     )
