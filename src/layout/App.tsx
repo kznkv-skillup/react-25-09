d60578d9fb3c43df5cd16fa86b138693b86926e2
Yaroslav <kznkv.test@gmail.com>
2024-01-11 19:44:40 +0200

    added Quantity

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -32,6 +32,13 @@ const App = (props: Props) => {
         setProductsInCart((prevState) => omit(prevState, id))
     }
 
+    const changeProductQuantity = (id: number, count: number) => {
+        setProductsInCart((prevState) => ({
+            ...prevState,
+            [id]: count,
+        }))
+    }
+
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
@@ -55,6 +62,7 @@ const App = (props: Props) => {
                         path="cart"
                         element={
                             <CartPage
+                                changeProductQuantity={changeProductQuantity}
                                 removeProductFromCart={removeProductFromCart}
                                 productsInCart={productsInCart}
                             />
