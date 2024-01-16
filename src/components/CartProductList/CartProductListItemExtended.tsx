08e4a2c8842ec0c052c952c7c2678ba20f269ae4
Yaroslav <kznkv.test@gmail.com>
2024-01-16 18:50:50 +0200

    added like logic to cart

--- src/components/CartProductList/CartProductListItemExtended.tsx
+++ src/components/CartProductList/CartProductListItemExtended.tsx
@@ -2,6 +2,10 @@ import { Button, Card, CardContent, Grid } from '@mui/material'
 import { Product } from 'utils/productsArray'
 import DeleteIcon from '@mui/icons-material/Delete'
 import Quantity from 'components/Quantity/Quantity'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import { useAppDispatch, useAppSelector } from '../../redux/hooks'
+import { toggleLikeState } from '../../redux/likeReducer'
 
 type Props = {
     product: Product
@@ -16,10 +20,21 @@ const CartProductListItemExtended = ({
     removeProductFromCart,
     changeProductQuantity,
 }: Props) => {
+    const isLiked = useAppSelector(
+        (state) => state.productsLikeState[product.id]
+    )
+    const dispatch = useAppDispatch()
+
     return (
         <Grid item xs={12} sm={6} md={4}>
             <Card variant="outlined">
                 <CardContent>
+                    <Button
+                        variant="outlined"
+                        onClick={() => dispatch(toggleLikeState(product.id))}
+                    >
+                        {isLiked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+                    </Button>
                     <div className="product-img">
                         <img src={product.image} alt={product.title} />
                     </div>
