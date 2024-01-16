f00b98dbc648718886dd9562e27ba5f7c5e655f1
Yaroslav <kznkv.test@gmail.com>
2024-01-16 20:38:54 +0200

    added useEffect for products

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -7,10 +7,23 @@ import { Container } from '@mui/material'
 import Home from 'pages/Home/Home'
 import CartPage from 'pages/CartPage/CartPage'
 import CheckoutPage from 'pages/Checkout/CheckoutPage'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 type Props = {}
 
 const App = (props: Props) => {
+    const [products, setProducts] = useState<any>([])
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data } = await axios.get(
+                'https://run.mocky.io/v3/1a22a9f2-a16e-4132-8e4d-a06cfae74ebf'
+            )
+            setProducts(data.products)
+        }
+        fetchData()
+    }, [])
+
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
